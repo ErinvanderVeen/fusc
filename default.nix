29cd32c2005eba43bdb62404d133a35ecf62a137
Erin van der Veen <erin@erinvanderveen.nl>
2021-04-02 16:49:25 +0200

    Use nix to build fusc
    Removes package dependencies for libraries

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, base, directory, lib, mtl, parsec, tasty
+, tasty-hunit
+}:
+mkDerivation {
+  pname = "fuspel-compiler";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [ base mtl parsec ];
+  executableHaskellDepends = [ base mtl parsec ];
+  testHaskellDepends = [ base directory parsec tasty tasty-hunit ];
+  description = "A compiler for the Fuspel language";
+  license = lib.licenses.gpl2Only;
+}
