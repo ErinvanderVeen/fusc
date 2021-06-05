c1cb5e192542650dd477e93623fa93120e027a9e
Erin van der Veen <erin@erinvanderveen.nl>
2021-06-05 20:38:09 +0200

    Reorganize nix files

--- default.nix
+++ default.nix
@@ -1,15 +1,4 @@
-{ mkDerivation, base, directory, lib, mtl, parsec, tasty
-, tasty-hunit
-}:
-mkDerivation {
-  pname = "fuspel-compiler";
-  version = "0.1.0.0";
-  src = ./.;
-  isLibrary = true;
-  isExecutable = true;
-  libraryHaskellDepends = [ base mtl parsec ];
-  executableHaskellDepends = [ base mtl parsec ];
-  testHaskellDepends = [ base directory parsec tasty tasty-hunit ];
-  description = "A compiler for the Fuspel language";
-  license = lib.licenses.gpl2Only;
-}
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.haskellPackages.callPackage ./nix/default.nix {}
